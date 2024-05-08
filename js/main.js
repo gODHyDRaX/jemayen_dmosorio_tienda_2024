31b68128acda21b68b2881a688a10b05626cb643
gODHyDRaX <jemayen@scl.edu.gt>
2024-05-08 13:01:08 -0600

    footer: contenido

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let root = document.querySelector(".root")
 root.innerHTML = `
-    <header class="header">Header</header>
+    <header class="header"></header>
     <div class="main"></div>
-    <footer class="footer">Footer</footer>
+    <footer class="footer"></footer>
 `
