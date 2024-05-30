8ebdc51603570262618abc54e304d94674699dec
gODHyDRaX <jemayen@scl.edu.gt>
2024-05-29 23:52:59 -0600

    subida_login

--- js/ventana_modal.js
+++ js/ventana_modal.js
@@ -1,14 +1,14 @@
+import { usuarioId } from "./login.js";
 let root = document.querySelector(".main");
 let btn_shop = document.querySelector(".btn_shop");
 
 btn_shop.addEventListener("click", async () => {
-    // Crear el modal
+
     let modal = document.createElement("div");
     modal.classList.add("modal");
     modal.style.display = 'block';
     root.appendChild(modal);
 
-    // Agregar contenido al modal
     modal.innerHTML = `
         <div class="tu_crt">
             Carrito
@@ -18,32 +18,25 @@ btn_shop.addEventListener("click", async () => {
         <div class="cpr">Comprar</div>
     `;
 
-    // Evento para cerrar el modal
     modal.querySelector('.close').addEventListener('click', () => {
         modal.remove();
     });
 
-    // Obtener y cargar productos en el carrito
-    await obtener_productos_carrito(2);
+    await obtener_productos_carrito(usuarioId);
 });
 
 async function obtener_productos_carrito(cartId) {
     try {
-        // Obtener los datos del carrito
         const cartResponse = await fetch(`https://fakestoreapi.com/carts/${cartId}`);
         const cartData = await cartResponse.json();
 
-        // Obtener los IDs de los productos en el carrito
         const productIds = cartData.products.map(product => product.productId);
 
-        // Obtener los detalles de los productos en el carrito
         const productResponse = await fetch('https://fakestoreapi.com/products');
         const productData = await productResponse.json();
 
-        // Filtrar los productos que están en el carrito
         const productsInCart = productData.filter(product => productIds.includes(product.id));
 
-        // Cargar productos en el carrito
         cargarCrt(productsInCart);
     } catch (error) {
         console.log("Error al obtener los productos del carrito:", error);
